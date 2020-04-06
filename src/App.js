1263ecf34cc3536d427a0ce4e81a98e0f87438f8
Lucas Miiller <lcorrea@unicadtec.com.br>
2020-04-06 13:14:43 -0300

    Configurando redux, redux saga e reactotron

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { Router } from 'react-router-dom';
 import history from './services/history';
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
+import { Provider } from 'react-redux';
+import store from './store';
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-            <GlobalStyle />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes />
+                <GlobalStyle />
+            </Router>
+        </Provider>
     );
 }
 
